fdb814d40326c3700d2716c3d4b0ed875bbc0712
Ebru Alici <ebrugulnaz@gmail.com>
2019-01-20 22:09:42 -0700

    Final commit

--- README.md
+++ README.md
@@ -1,15 +1,16 @@
 ## Jets Application
 
 Description
+	This is a program that you can add jets to an airfield. There are three types of jets, basic, fighter and cargo carrier.  All the jets can fly and the cargo jets can load cargo and fighter jets can fight. The program starts with five jets and it can have up to ten jets, which can be added by the user.
 
 ##Technologies
 * Java
 * Object-Oriented design
-  *  Polymorphism
+  * Polymorphism
   * Inheritance
   * Abstraction
   * Encapsulation
-* UML
 
 
-Image, maybe the UML I downloaded from Skill Distillery's Github page.
+
+![jet picture](http://theinventionofthejetengine.weebly.com/uploads/1/5/9/2/15921090/3708091_orig.jpg)
--- src/com/skilldistillary/jets/JetsApplication.java
+++ src/com/skilldistillary/jets/JetsApplication.java
@@ -7,49 +7,52 @@ public class JetsApplication {
 	public static void main(String[] args) {
 		Scanner keyboard = new Scanner(System.in);
 
-		int numberEntered = 0;
 
-		AirFields airport = new AirFields();
-		CargoPlane cargo = new CargoPlane();
+		AirFields airport = new AirFields();// creating airfield to add jets to
+		
+		
+		CargoPlane cargo = new CargoPlane();// creating a cargo plane and setting it's values
 		cargo.setModel("737-800");
 		cargo.setPrice(78_000_000);
 		cargo.setRange(2935);
 		cargo.setSpeed(842);
 
-		Jets passenger = new JetImpl();
+		Jets passenger = new JetImpl();// creating a basic jet and setting it's values
 		passenger.setModel("737-700");
 		passenger.setPrice(80_000_000);
 		passenger.setRange(3010);
 		passenger.setSpeed(834);
 
-		FighterJets fighters = new FighterJets();
+		FighterJets fighters = new FighterJets();// creating a FighterJet and setting it's values
 		fighters.setModel("F-16");
 		fighters.setPrice(14_000_000);
 		fighters.setRange(2022);
 		fighters.setSpeed(1500);
 
-		Jets business = new JetImpl();
+		Jets business = new JetImpl();// creating a basic Jet and setting it's values
 		business.setModel("Lear Jet 75");
 		business.setPrice(13_000_000);
 		business.setRange(2000);
 		business.setSpeed(800);
 
-		Jets transport = new JetImpl();
+		Jets transport = new JetImpl();// creating a generic Jet and setting it's values
 		transport.setModel("C-17 Globemaster III");
 		transport.setPrice(218_000_000);
 		transport.setRange(2400);
 		transport.setSpeed(830);
 
-		airport.addJet(cargo);
-		airport.addCargo(cargo);
-		airport.addJet(passenger);
-		airport.addJet(fighters);
-		airport.addFighter(fighters);
+		airport.addJet(cargo);// adding cargo to jet list and CargoCarrier list
+		airport.addCargo(cargo);// cargo is a jet and a CargoCarrier
+		
+		airport.addJet(fighters);// adding fighters to jet list and CombatReady list
+		airport.addFighter(fighters);// fighters is a jet and a CombatReady
+		
+		airport.addJet(passenger);// addding 3 basic jets to jet list
 		airport.addJet(business);
 		airport.addJet(transport);
 
-		// System.out.println(airport.getJetList()[1]);
-		// System.out.println(cargo.setModel);
+		
+		// next part displays menu to users
 		System.out.println("For a list of jets. ");
 		System.out.println("Enter: 1");
 		System.out.println("To fly all jets. ");
@@ -66,8 +69,8 @@ public class JetsApplication {
 		System.out.println("Enter: 7");
 		System.out.println("To Quit");
 		System.out.println("Enter: 8");
-		int num = 0;
-		do {
+		int num = 0;  // create num here so it would work in while loop
+		do { // loop to take input and keep going till users hit 8
 
 			num = keyboard.nextInt();
 			if (num == 1) {
@@ -101,14 +104,7 @@ public class JetsApplication {
 			}
 		} while (num != 8);
 	}
-	
-
-
-
-
-
-
-
+	// goes through list of jets and prints out jets using toString of jet
 	public static void printJets(AirFields airport) {
 		Jets[] jetList = airport.getJetList();
 		for(int i = 0; i < jetList.length; i++) {
@@ -118,6 +114,7 @@ public class JetsApplication {
 		}
 	}
 
+	// goes through list of jets and calls their fly
 	public static void flyJets(AirFields airport) {
 		Jets[] jetList = airport.getJetList();
 		for(int i = 0; i < jetList.length; i++) {
@@ -128,6 +125,7 @@ public class JetsApplication {
 		}
 	} 
 	
+	// goes through jet and finds the fastest 
 	public static void getFastestJet(AirFields airport) {
 		Jets[] jetList = airport.getJetList();
 		Jets fast = jetList[0];
@@ -140,7 +138,7 @@ public class JetsApplication {
 		System.out.println(fast);
 		
 	}
-	
+	//goes thru jets to fine the longest range
 	public static void getLongestRangeJet(AirFields airport) {
 		Jets[] jetList = airport.getJetList();
 		Jets longest = jetList[0];
@@ -153,6 +151,7 @@ public class JetsApplication {
 		System.out.println(longest);
 		
 	}
+	//adding a jet from user input
 	private static Jets addJet(Scanner keyboard) {
 		Jets newJetAdded = new JetImpl();
 		System.out.println("Enter Model: ");
@@ -170,7 +169,7 @@ public class JetsApplication {
 		return newJetAdded;
 		
 	}
-
+	//loading cargo to cargo carrier
 	private static void loadAllCargoJets(AirFields airport) {
 		CargoCarrier[] jetList = airport.getCargoJets();
 		for(int i = 0; i < jetList.length; i++) {
@@ -180,7 +179,7 @@ public class JetsApplication {
 			}
 		}
 	}
-	
+	//fighting fighters
 	private static void dogFigth(AirFields airport) {
 		CombatReady[] jetList = airport.getFighterJets();
 		for(int i = 0; i < jetList.length; i++) {
